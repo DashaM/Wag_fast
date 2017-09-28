df84b76cad4b2c6ca1e66435d29307dcc242645f
daria.minina@cactussoft.biz <daria.minina@cactussoft.biz>
2017-09-28 17:52:24 +0300

    new enemo product

--- WAG_tests/WAG_tests/UITests/Price.cs
+++ WAG_tests/WAG_tests/UITests/Price.cs
@@ -53,7 +53,7 @@ namespace WAG_fast
         [Test()]
         public void PriceCheckEnemoSe()
         {
-            GoToProductPage("https://www.enemo.se/vitvaror/tvattmaskiner/frontmatad-tvattmaskin/hoover-dmp-413-ah/");
+            GoToProductPage("https://www.enemo.se/vitvaror/tvattmaskiner/frontmatad-tvattmaskin/product/bosch-wat283l8sn/");
             FindPrice();
         }
 
